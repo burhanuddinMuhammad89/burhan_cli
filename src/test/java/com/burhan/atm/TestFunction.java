38cb48805d48487a19b5dca68f45ab48620ed64b
Muhammad Burn <burhannudin@moladin.com>
2023-03-12 17:13:22 +0700

    Unit Test

---
+++ src/test/java/com/burhan/atm/TestFunction.java
@@ -0,0 +1,28 @@
+package com.burhan.atm;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.burhan.atm.controller.AtmController;
+import com.burhan.atm.model.Account;
+
+import redis.clients.jedis.Jedis;
+
+public class TestFunction {
+    Account result = new Account();
+    Jedis jedis = new Jedis("localhost", 6379);
+    AtmController atmController = new AtmController(jedis);
+    @Test  
+    public void testAtm() throws Exception{  
+        result.setUserName("alice");
+        result.setAmountBalance(20.0);
+        assertTrue(atmController.login("bob") instanceof Account);  
+        assertTrue(atmController.deposit(20.0) instanceof String);  
+        assertTrue(atmController.transfer(result) instanceof Account);
+        assertTrue(atmController.logout() instanceof String);
+    }  
+}
